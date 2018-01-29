5a1cdc98ea0ca11b60d1a0270e8c983b86eee16c
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-01-28 20:15:33 -0600

    associations - added supplier

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
+  belongs_to :supplier
+
   validates :name, presence: true
   validates :name, uniqueness: true
-
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
-
   validates :description, length: { in: 10..500 }
 
   def discounted?
@@ -18,17 +18,13 @@ class Product < ApplicationRecord
   def total
     tax + price
   end
-
-  def as_json
-    {
-      id: id,
-      name: name,
-      image_url: image_url,
-      description: description,
-      price: price,
-      is_discounted: discounted?,
-      tax: tax,
-      total: total
-    }
-  end
 end
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get '/products/:id' => 'products#show'
   patch '/products/:id' => 'products#update'
   delete '/products/:id' => 'products#destroy'
+
+  get '/suppliers' => 'suppliers#index'
+  post '/suppliers' => 'suppliers#create'
+  get '/suppliers/:id' => 'suppliers#show'
+  patch '/suppliers/:id' => 'suppliers#update'
+  delete '/suppliers/:id' => 'suppliers#destroy'
 end
 
-
+# http://localhost:3000/products?search=soccer
