04dabe8ccb88eaf84fc8de2fd81488dc86f0b50e
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-02-20 21:34:13 -0600

    new and edit routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# Category.create(name: "electronics")
-# Category.create(name: "furniture")
-# Category.create(name: "misc")
-
-# CategoryProduct.create([
-#                         {category_id: 1, product_id: 2},
-#                         {category_id: 1, product_id: 5},
-#                         {category_id: 2, product_id: 2},
-#                         {category_id: 2, product_id: 4},
-#                         {category_id: 2, product_id: 6},
-#                         {category_id: 3, product_id: 1}
-#                         ])
+# Supplier.create!([
+#   {name: "Big Bob's Novelty Emporium", email: "bigbob@emporium.com", phone_number: "312-555-0864"},
+#   {name: "Ted's Toys and Stuff", email: "ted@gmail.com", phone_number: "818-555-1357"},
+#   {name: "Phil's Van Down By the River", email: "van4627@yahoo.com", phone_number: "444-van-dude"}
+# ])
 
 # Product.create!([
-#   {name: "WNYX Mug", price: "1.50", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape", supplier_id: 1},
-#   {name: "Lightsaber", price: "270.10", description: "Part laser, part samuri sword, all awesome. The lightsaber is an elogant weapon for a more civilized age, not nearly as clumsy as a blaster", supplier_id: 3},
-#   {name: "Space Cowboy Laser Gun", price: "170.89", description: "This weapon has no other description than, Shiney!", supplier_id: 2},
-#   {name: "DnD Dice set", price: "57.0", description: "Take down mighty dragons with this timeless set of 20 sided wonders", supplier_id: 1},
-#   {name: "Hitchhiker's Guide to the Galexy", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover.", supplier_id: 1, price: 42.00 },
-#   {name: "Sonic Screwdriver", price: "8.99", description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood", supplier_id: 3}
+#   {name: "WNYX Mug", price: "1.50", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape", supplier_id: 14},
+#   {name: "Lightsaber", price: "270.10", description: "Part laser, part samuri sword, all awesome. The lightsaber is an elogant weapon for a more civilized age, not nearly as clumsy as a blaster", supplier_id: 13},
+#   {name: "Space Cowboy Laser Gun", price: "170.89", description: "This weapon has no other description than, Shiney!", supplier_id: 14},
+#   {name: "DnD Dice set", price: "57.0", description: "Take down mighty dragons with this timeless set of 20 sided wonders", supplier_id: 14},
+#   {name: "Hitchhiker's Guide to the Galexy", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover.", supplier_id: 13, price: 42.00 },
+#   {name: "Sonic Screwdriver", price: "8.99", description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood", supplier_id: 15}
 # ])
+
+# Image.create!([
+#   {url: "http://s32.postimg.org/6mceui22t/wnyx_mug.png", product_id: 15},
+#   {url: "http://25.media.tumblr.com/d2456964024018fd930338c099371104/tumblr_n2m73lTx2Q1svn82uo1_400.gif", product_id: 16},
+#   {url: "http://cdn.shopify.com/s/files/1/0289/1534/products/MalPistol_MP-1_1756x988_e53f9448-81ec-41de-9369-4cbad64f18f5_1024x1024.jpg?v=1401915776", product_id: 17},
+#   {url: "https://s-media-cache-ak0.pinimg.com/736x/9c/15/7b/9c157bea5331463f9c539cbce739a4b8.jpg", product_id: 18},
+#   {url: "http://www.notcot.com/images/guide.gif", product_id: 19},
+#   {url: "https://dyn0.media.forbiddenplanet.com/products/28577492.jpg", product_id: 20},
+  # {url: "https://lumiere-a.akamaihd.net/v1/images/Lightsaber_853fb596.jpeg?region=1%2C0%2C1444%2C812&width=768", product_id: 21},
+  # {url: "https://ksr-ugc.imgix.net/assets/011/249/310/b80ad66b11f09e88161908bf726ed68c_original.jpg?crop=faces&w=1552&h=873&fit=crop&v=1465459663&auto=format&q=92&s=f93c49497b6ec22bd8e4c848c49a7d94", product_id: 22}
+# ])
+
+
+# Category.create!([
+#                   {name: "Wearable"},
+#                   {name: "Expensive"},
+#                   {name: "Cheap"},
+#                   {name: "Weapon"}
+#                   ])
+
+# CategoryProduct.create!([
+#                         {category_id: 7, product_id: 16},
+#                         {category_id: 7, product_id: 17},
+#                         {category_id: 8, product_id: 18},
+#                         {category_id: 8, product_id: 19},
+#                         {category_id: 9, product_id: 20},
+#                         {category_id: 9, product_id: 15},
+#                         {category_id: 9, product_id: 16},
+#                         {category_id: 10, product_id: 16},
+#                         {category_id: 10, product_id: 15},
+#                         {category_id: 10, product_id: 20},
+#                         {category_id: 10, product_id: 20}
+#                         ])
--- public/index.js
+++ public/index.js
@@ -1,20 +1,165 @@
-var productTemplate = document.querySelector("#product-card");
-var productContainer = document.querySelector(".row");
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "This is my sample Vue.js page!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 
-// productContainer.appendChild(productTemplate.content.cloneNode(true));
-// productContainer.appendChild(productTemplate.content.cloneNode(true));
-// productContainer.appendChild(productTemplate.content.cloneNode(true));
+var RandomPage = {
+  template: "#random-page",
+  data: function() {
+    return {
+      message: "This is my random Vue.js page!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 
-axios.get("http://localhost:3000/products").then(function(response) {
-  var products = response.data;
-  products.forEach(function(product) {
-    var productClone = productTemplate.content.cloneNode(true);
-    // productClone.querySelector(".card-title").innerText = product.title;
-    productClone.querySelector(".name").innerText = product.name;
-    productClone.querySelector(".description").innerText = product.description;
-    productClone.querySelector(".description").innerText = product.description;
-    productContainer.appendChild(productClone);
-  });
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LogoutPage = {
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
+var ProductsNewPage = {
+  template: "#products-new-page",
+  data: function() {
+    return {
+      name: "",
+      price: "",
+      description: "",
+      supplier_id: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        supplier_id: this.supplierId
+      };
+      axios
+        .post("/products", params)
+        .then(function(response) {
+          router.push("/");
+          // this is the happy path, it directs to the main page
+        })
+        .catch(
+          function(error) {
+            console.log(error.response.data);
+            this.errors = error.response.data.errors;
+            router.push("/login");
+            // this is the sad path where you will be routed to if login unsucessful (errors will be shown)
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var router = new VueRouter({
+  routes: [
+            { path: "/", component: SamplePage },
+            { path: "/random", component: RandomPage },
+            { path: "/signup", component: SignupPage },
+            { path: "/login", component: LoginPage },
+            { path: "/logout", component: LogoutPage },
+            { path: "/products/new", component: ProductsNewPage },
+          ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
 });
 
 
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+      var jwt = localStorage.getItem("jwt");
+      if (jwt) {
+        axios.defaults.headers.common["Authorization"] = jwt;
+      }
+    }
+});
+
+
+// productContainer.appendChild(productTemplate.content.cloneNode(true));
+// productContainer.appendChild(productTemplate.content.cloneNode(true));
+// productContainer.appendChild(productTemplate.content.cloneNode(true));
