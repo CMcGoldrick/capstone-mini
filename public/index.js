390944063dfc545bf1e6f9ab2b3d920be77a7562
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-02-22 21:30:55 -0600

    Vue.js new/create/edit action

--- public/index.js
+++ public/index.js
@@ -1,20 +1,44 @@
+var ProductsIndexPage = {
+  template: "#products-index-page",
+  data: function() {
+    return {
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/products")
+    .then(function(response) {
+      this.products = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
+var ProductsShowPage = {
+  template: "#products-show-page",
+  data: function() {
+    console.log("help");
+    return {
+      product: {
+        name: "hello",      
+        description: "bob"
+      }
+    }
+  },
+  created: function() {
+    axios.get("/products/" + this.$route.params.id )
+    .then(function(response) {
+      this.product = response.data;
+    }.bind(this));    
+  }
+};
+
 var SamplePage = {
   template: "#sample-page",
   data: function() {
     return {
       message: "This is my sample Vue.js page!"
-    };
-  },
-  created: function() {},
-  methods: {},
-  computed: {}
-};
-
-var RandomPage = {
-  template: "#random-page",
-  data: function() {
-    return {
-      message: "This is my random Vue.js page!"
     };
   },
   created: function() {},
@@ -133,14 +157,55 @@ var ProductsNewPage = {
   }
 };
 
+var ProductsEditPage = {
+  template: "#products-edit-page",
+  data: function() {
+    return {
+      name: "",
+      description: "",
+    };
+  },
+  created: function() {
+    axios
+      .get("/products/" + this.$route.params.id)
+      .then(function(response) {
+        console.log(response.data);
+        var product = response.data;
+        this.name = product.name
+        this.description = product.description
+      }.bind(this));
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        description: this.description
+      };
+      axios
+        .patch("/products/" + this.$route.params.id, params)
+        .then(function(response) {
+          router.push("/products/" + response.data.id);
+        }.bind(this))
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+            router.push("/login");
+          });
+    }
+  }
+};
+
+
 var router = new VueRouter({
   routes: [
-            { path: "/", component: SamplePage },
-            { path: "/random", component: RandomPage },
+            { path: "/", component: ProductsIndexPage},
+            { path: "/products", component: ProductsIndexPage},
+            { path: "/products/new", component: ProductsNewPage },
+            { path: "/products/:id", component: ProductsShowPage },
+            { path: "/products/:id/edit", component: ProductsEditPage },
             { path: "/signup", component: SignupPage },
             { path: "/login", component: LoginPage },
             { path: "/logout", component: LogoutPage },
-            { path: "/products/new", component: ProductsNewPage },
           ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
@@ -158,7 +223,6 @@ var app = new Vue({
       }
     }
 });
-
 
 // productContainer.appendChild(productTemplate.content.cloneNode(true));
 // productContainer.appendChild(productTemplate.content.cloneNode(true));
